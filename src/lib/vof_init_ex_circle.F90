b3a938f0bd5a46f5f4080479b1eff2f21f86959d
Zach Jibben <zjibben@lanl.gov>
2017-08-14 15:17:00 -0600

    Fix exact circle initialization

--- src/lib/vof_init_ex_circle.F90
+++ src/lib/vof_init_ex_circle.F90
@@ -22,8 +22,8 @@ contains
     integer :: i
 
     do i = 1,mesh%ncell
-      vof(1,i) = cell_vof(mesh%x(:,mesh%cnode(:,i)), R)
-      vof(2,i) = 1 - vof(1,i)
+      vof(2,i) = cell_vof(mesh%x(:,mesh%cnode(:,i)), R)
+      vof(1,i) = 1 - vof(2,i)
     end do
 
   end subroutine vof_init_circle
@@ -42,33 +42,33 @@ contains
     volume = (xb(1,2) - xb(1,1)) * (xb(2,2) - xb(2,1))
 
     ! switch to quadrant 1 if the cell doesn't cross quadrants
-    if (count(xb(:,1) < 0) == 2) then
+    if (count(xb(1,:) < 0) == 2) then
       xp = xb(1,1)
-      xb(1,1) = -xb(2,1)
-      xb(2,1) = -xp
+      xb(1,1) = -xb(1,2)
+      xb(1,2) = -xp
     end if
-    if (count(xb(:,2) < 0) == 2) then
-      xp = xb(1,2)
-      xb(1,2) = -xb(2,2)
+    if (count(xb(2,:) < 0) == 2) then
+      xp = xb(2,1)
+      xb(2,1) = -xb(2,2)
       xb(2,2) = -xp
     end if
 
     ! check which points are inside the circle
     is_inside(1) = isInside(xb(:,1), R)
-    is_inside(2) = isInside([xb(1,1), xb(2,2)], R)
+    is_inside(2) = isInside([xb(1,2), xb(2,1)], R)
     is_inside(3) = isInside(xb(:,2), R)
-    is_inside(4) = isInside([xb(1,2), xb(2,1)], R)
+    is_inside(4) = isInside([xb(1,1), xb(2,2)], R)
     ninside = count(is_inside)
 
     select case (ninside)
     case (0)
       cell_vof = 0
 
-      if (xb(2,1) < R) then
+      if (xb(2,1) < R .and. xb(1,1) < 0 .and. xb(1,2) > 0) then
         xp = sqrt(R**2 - xb(2,1)**2)
         cell_vof = antiderivative(xp,R) - antiderivative(-xp,R)
         cell_vof = cell_vof - 2 * xp * xb(2,1) ! subtract off the area outside the cell
-      else if (xb(1,1) < R) then
+      else if (xb(1,1) < R .and. xb(2,1) < 0 .and. xb(2,2) > 0) then
         xp = sqrt(R**2 - xb(1,1)**2)
         cell_vof = antiderivative(xp,R) - antiderivative(-xp,R)
         cell_vof = cell_vof - 2 * xp * xb(1,1) ! subtract off the area outside the cell
@@ -97,11 +97,26 @@ contains
     end select
     cell_vof = cell_vof / volume
 
+    if (cell_vof > 1 .or. cell_vof < 0) then
+
+      print *, cell_vof
+      print *, ninside, is_inside
+      print *, xb(:,1)
+      print *, xb(:,2)
+      print *, xp
+
+      call LS_Fatal ("vof init error")
+    end if
+
+    ! print *, is_inside
+    ! print *, cell_vof
+    ! stop
+
   end function cell_vof
 
   pure real(r8) function antiderivative(x, R)
     real(r8), intent(in) :: x, R
-    antiderivative = (R * sqrt(R**2 - x**2) - x**2 * log(sqrt(R**2 - x**2) + R)) / 2
+    antiderivative = (x * sqrt(R**2 - x**2) + R**2 * atan(x/sqrt(R**2 - x**2))) / 2
   end function antiderivative
 
   pure logical function isInside(x,R)
