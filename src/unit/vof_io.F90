02d583e79e4f0176f43c6ea8e003a46476b08883
Zach Jibben <zjibben@lanl.gov>
2017-08-14 12:27:13 -0600

    Add vof save/store routines

---
+++ src/unit/vof_io.F90
@@ -0,0 +1,39 @@
+module vof_io
+
+  use kinds, only: r8
+  implicit none
+
+contains
+
+  subroutine store_vof_field(filename, vof)
+
+    character(*), intent(in) :: filename
+    real(r8), intent(in) :: vof(:,:)
+
+    integer :: fh
+
+    open(newunit=fh, file=filename, action='write', access='stream')
+    write(fh) vof
+    close(fh)
+
+  end subroutine store_vof_field
+
+  subroutine read_vof_field(filename, vof)
+
+    character(*), intent(in) :: filename
+    real(r8), intent(out) :: vof(:,:)
+
+    integer :: fh
+
+    open(newunit=fh, file=filename, action='read', access='stream')
+    read(fh) vof
+    close(fh)
+
+  end subroutine read_vof_field
+
+  logical function file_exists(filename)
+    character(*), intent(in) :: filename
+    inquire(file=filename, exist=file_exists)
+  end function file_exists
+
+end module vof_io
